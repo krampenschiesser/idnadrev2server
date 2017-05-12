b21f8c823bb28e8b43cd3598abae73976fa7814b
krampenschiesser <krampenschiesser@gmail.com>
2017-05-12 06:28:59 +0200

    cache hash in global ui state

--- src/rest/ui.rs
+++ src/rest/ui.rs
@@ -15,20 +15,17 @@ use rocket::response::Redirect;
 use rocket::http::{Status, ContentType};
 use std::io::Cursor;
 
-use super::super::UiDir;
+use state::UiState;
 
 #[get("/")]
-pub fn index(ui_dir: State<UiDir>) -> io::Result<NamedFile> {
-    info!("{:?}", ui_dir);
-    NamedFile::open(ui_dir.0.join("index.html"))
+pub fn index(state: State<UiState>) -> io::Result<NamedFile> {
+    info!("{:?}", state.ui_dir);
+    NamedFile::open(state.ui_dir.join("index.html"))
 }
 
 #[get("/manifest.appcache")]
-pub fn manifest(ui_dir: State<UiDir>) -> Response {
-    let dir = &ui_dir.0;
-
-    let hash: ::std::io::Result<String> = hash_dir(&dir);
-
+pub fn manifest(state: State<UiState>) -> Response {
+    let hash = state.compute_hash();
     let status = if hash.is_ok() { Status::Ok } else { Status::InternalServerError };
 
     let body = if hash.is_ok() { hash.unwrap() } else { format!("{}", hash.err().unwrap()) };
@@ -41,125 +38,29 @@ pub fn manifest(ui_dir: State<UiDir>) -> Response {
 }
 
 #[get("/static/<file..>", rank = 9)]
-pub fn files(file: PathBuf, ui_dir: State<UiDir>) -> Option<NamedFile> {
-    NamedFile::open(ui_dir.0.join("static").join(file)).ok()
+pub fn files(file: PathBuf, state: State<UiState>) -> Option<NamedFile> {
+    NamedFile::open(state.ui_dir.join("static").join(file)).ok()
 }
 
 #[get("/asset-manifest.json")]
-pub fn asset_mainfest(ui_dir: State<UiDir>) -> Option<NamedFile> {
-    NamedFile::open(ui_dir.0.join("asset-manifest.json")).ok()
+pub fn asset_mainfest(state: State<UiState>) -> Option<NamedFile> {
+    NamedFile::open(state.ui_dir.join("asset-manifest.json")).ok()
 }
 
 #[get("/favicon.ico")]
-pub fn favicon(ui_dir: State<UiDir>) -> Option<NamedFile> {
-    NamedFile::open(ui_dir.0.join("favicon.ico")).ok()
+pub fn favicon(state: State<UiState>) -> Option<NamedFile> {
+    NamedFile::open(state.ui_dir.join("favicon.ico")).ok()
 }
 
 #[get("/index.html")]
-pub fn index_html(ui_dir: State<UiDir>) -> Option<NamedFile> {
-    NamedFile::open(ui_dir.0.join("index.html")).ok()
+pub fn index_html(state: State<UiState>) -> Option<NamedFile> {
+    NamedFile::open(state.ui_dir.join("index.html")).ok()
 }
 
 #[get("/<any..>", rank = 10)]
 //pub fn any(any: PathBuf, ui_dir: State<UiDir>) -> Option<NamedFile> {
 //    NamedFile::open(ui_dir.0.join("index.html")).ok()
 //}
-pub fn any(any: PathBuf, ui_dir: State<UiDir>) -> Redirect {
+pub fn any(any: PathBuf, state: State<UiState>) -> Redirect {
     Redirect::to("/")
-}
-
-fn read_dir(root: &Path) -> ::std::io::Result<Vec<PathBuf>> {
-    let dir = root.read_dir()?;
-
-    let mut v = Vec::new();
-
-    for entry in dir {
-        let entry = entry?;
-        if entry.file_type()?.is_dir() {
-            let mut sub = read_dir(&entry.path().to_path_buf())?;
-            v.append(&mut sub);
-        } else {
-            v.push(entry.path().to_path_buf());
-        }
-    }
-    Ok(v)
-}
-
-fn hash(paths: &[PathBuf]) -> ::std::io::Result<String> {
-    use std::fs::File;
-    use std::io::Read;
-
-    let mut sha1 = ::sha1::Sha1::new();
-    for path in paths {
-        let mut buf = Vec::new();
-        File::open(path)?.read_to_end(&mut buf)?;
-        sha1.update(buf.as_slice());
-    }
-    Ok(sha1.digest().to_string())
-}
-
-fn hash_dir(root: &PathBuf) -> ::std::io::Result<String> {
-    let paths = read_dir(root)?;
-
-    let hash = hash(&paths)?;
-    let mut names = Vec::new();
-    for path in paths {
-        names.push(path_to_string(root, &path))
-    }
-    let mut retval = String::new();
-    retval.push_str("CACHE MANIFEST\n");
-    retval.push_str("#");
-    retval.push_str(hash.as_str());
-    retval.push_str("\nCACHE:\n");
-    for name in names {
-        retval.push_str(name.as_str());
-        retval.push_str("\n");
-    }
-    retval.push_str("\nFALLBACK:\n");
-    retval.push_str("/ /index.html\n");
-
-    retval.push_str("\nNETWORK:\n*\n");
-
-    Ok(retval)
-}
-
-fn path_to_string(root: &PathBuf, path: &PathBuf) -> String {
-    let mut str = String::new();
-    let prefix = root.as_os_str().to_string_lossy();
-
-    for p in path {
-        let p = p.to_string_lossy().into_owned();
-        if !prefix.contains(p.as_str()) {
-            str.push_str(format!("/{}", p).as_str());
-        }
-    }
-    str
-}
-
-#[cfg(test)]
-mod tests {
-    use super::hash_dir;
-    use tempdir::TempDir;
-    use std::fs::{File, create_dir};
-    use spectral::prelude::*;
-
-    #[test]
-    fn test_hash_dir() {
-        let temp = TempDir::new("hashing").unwrap();
-        let root = temp.path().to_path_buf();
-
-        let subdir = root.join("subdir");
-        create_dir(&subdir);
-
-        let file1 = root.join("file1.txt");
-        let file2 = subdir.join("file2.txt");
-
-        File::create(file1);
-        File::create(file2);
-
-        let hash = hash_dir(&root).unwrap();
-        println!("{}", hash);
-        assert_that(&hash).contains("/subdir/file2.txt");
-        assert_that(&hash).contains("/file1.txt");
-    }
 }
--- src/state/mod.rs
+++ src/state/mod.rs
@@ -8,14 +8,55 @@
 // except according to those terms.
 
 use crypt::{CryptoActor, CryptoSender, CryptError, AccessToken};
-use std::path::PathBuf;
+use std::path::{PathBuf, Path};
 use uuid::Uuid;
 use search::SearchCache;
 use crypt::CryptoIfc;
+use std::sync::RwLock;
 
 pub struct GlobalState {
     crypt_actor: CryptoActor,
     search_cache: SearchCache,
+
+}
+
+pub struct UiState {
+    pub ui_dir: PathBuf,
+    hash: RwLock<Hash>,
+}
+
+struct Hash { value: Option<String> }
+
+impl Hash {
+    fn new() -> Self {
+        Hash { value: None }
+    }
+}
+
+impl UiState {
+    pub fn new(ui_dir: PathBuf) -> Self {
+        UiState { ui_dir: ui_dir, hash: RwLock::new(Hash::new()) }
+    }
+
+    pub fn compute_hash(&self) -> ::std::io::Result<String> {
+        {
+            let ref o = self.hash.read().unwrap();
+            if o.value.is_some() {
+                let str = o.value.clone().unwrap();
+                return Ok(str)
+            }
+        }
+
+        let hash: ::std::io::Result<String> = hash_dir(&self.ui_dir);
+        match hash {
+            Ok(hash) => {
+                let ref mut o = self.hash.write().unwrap();
+                o.value = Some(hash.clone());
+                Ok(hash)
+            }
+            Err(e) => Err(e)
+        }
+    }
 }
 
 impl GlobalState {
@@ -29,11 +70,108 @@ impl GlobalState {
     pub fn crypt(&self) -> &CryptoActor {
         &self.crypt_actor
     }
-    pub fn search_cache(&self) -> &SearchCache{
+    pub fn search_cache(&self) -> &SearchCache {
         &self.search_cache
     }
 
     pub fn check_token(&self, repo: &Uuid, token: &AccessToken) -> bool {
         self.crypt_actor.check_token(repo, token)
+    }
+}
+
+
+fn read_dir(root: &Path) -> ::std::io::Result<Vec<PathBuf>> {
+    let dir = root.read_dir()?;
+
+    let mut v = Vec::new();
+
+    for entry in dir {
+        let entry = entry?;
+        if entry.file_type()?.is_dir() {
+            let mut sub = read_dir(&entry.path().to_path_buf())?;
+            v.append(&mut sub);
+        } else {
+            v.push(entry.path().to_path_buf());
+        }
+    }
+    Ok(v)
+}
+
+fn hash(paths: &[PathBuf]) -> ::std::io::Result<String> {
+    use std::fs::File;
+    use std::io::Read;
+
+    let mut sha1 = ::sha1::Sha1::new();
+    for path in paths {
+        let mut buf = Vec::new();
+        File::open(path)?.read_to_end(&mut buf)?;
+        sha1.update(buf.as_slice());
+    }
+    Ok(sha1.digest().to_string())
+}
+
+fn hash_dir(root: &PathBuf) -> ::std::io::Result<String> {
+    let paths = read_dir(root)?;
+
+    let hash = hash(&paths)?;
+    let mut names = Vec::new();
+    for path in paths {
+        names.push(path_to_string(root, &path))
+    }
+    let mut retval = String::new();
+    retval.push_str("CACHE MANIFEST\n");
+    retval.push_str("#");
+    retval.push_str(hash.as_str());
+    retval.push_str("\nCACHE:\n");
+    for name in names {
+        retval.push_str(name.as_str());
+        retval.push_str("\n");
+    }
+    retval.push_str("\nFALLBACK:\n");
+    retval.push_str("/ /index.html\n");
+
+    retval.push_str("\nNETWORK:\n*\n");
+
+    Ok(retval)
+}
+
+fn path_to_string(root: &PathBuf, path: &PathBuf) -> String {
+    let mut str = String::new();
+    let prefix = root.as_os_str().to_string_lossy();
+
+    for p in path {
+        let p = p.to_string_lossy().into_owned();
+        if !prefix.contains(p.as_str()) {
+            str.push_str(format!("/{}", p).as_str());
+        }
+    }
+    str
+}
+
+#[cfg(test)]
+mod tests {
+    use super::hash_dir;
+    use tempdir::TempDir;
+    use std::fs::{File, create_dir};
+    use spectral::prelude::*;
+
+    #[test]
+    fn test_hash_dir() {
+        let temp = TempDir::new("hashing").unwrap();
+        let root = temp.path().to_path_buf();
+
+        let subdir = root.join("subdir");
+        create_dir(&subdir);
+
+        let file1 = root.join("file1.txt");
+        let file2 = subdir.join("file2.txt");
+
+        File::create(file1);
+        File::create(file2);
+
+        let hash = hash_dir(&root).unwrap();
+        println!("{}", hash);
+        assert_that(&hash).contains("/subdir/file2.txt");
+        assert_that(&hash).contains("/file1.txt");
     }
 }
