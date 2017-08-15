2ff7964acaadc3a6b09a9de6767b8d107a49d0e6
loehnert <christian.loehnert@ssi-schaefer.com>
2017-08-14 20:43:58 -0400

    start using rest in rust

--- src/main.rs
+++ src/main.rs
@@ -11,13 +11,6 @@
 #![feature(custom_attribute)]
 #![feature(custom_derive)]
 
-extern crate iron;
-extern crate router;
-//extern crate urlencoded;
-extern crate persistent;
-extern crate mount;
-extern crate bodyparser;
-
 
 extern crate uuid;
 extern crate chrono;
@@ -35,22 +28,18 @@ extern crate tempdir;
 extern crate notify;
 #[macro_use]
 extern crate log;
-#[macro_use]
-extern crate mime;
-extern crate hyper;
 extern crate log4rs;
 extern crate distance;
 extern crate thread_local;
 extern crate rayon;
 extern crate sha1;
-extern crate unicase;
+extern crate rest_in_rust;
 
 #[cfg(test)]
 extern crate spectral;
 #[cfg(test)]
 extern crate reqwest;
 
-mod ironext;
 mod dto;
 mod search;
 mod crypt;
@@ -71,57 +60,43 @@ use crypt::actor::communication::{CryptResponse, CryptCmd};
 #[derive(Debug)]
 pub struct UiDir(PathBuf);
 
-use iron::prelude::*;
-use router::Router;
-use mount::Mount;
-use persistent::Read;
+use rest_in_rust::prelude::*;
 
 fn main() {
     log4rs::init_file("config/log4rs.yaml", Default::default()).unwrap();
 
-    let mut mount = Mount::new();
-    let mut repo_router = Router::new();
+    let mut router = Router::new();
+    router.get("/rest/v1/", rest::list_repositories);
+    router.post("/rest/v1/repo", rest::create_repository);
+    router.post("/rest/v1/repo/:repo_id", rest::open_repository);
+    router.get("/rest/v1/repo/:repo_id/file", rest::create_file);
+    router.get("/rest/v1/repo/:repo_id", rest::list_files);
+    router.get("/rest/v1/repo/:repo_id/", rest::list_files);
+    router.get("/rest/v1/repo/:repo_id/:type", rest::list_files);
+    router.get("/rest/v1/repo/:repo_id/:type/", rest::list_files);
 
-    repo_router.get("/", rest::list_repositories, "list_repositories");
-    repo_router.post("/repo", rest::create_repository, "create_repo");
-    repo_router.post("/repo/:repo_id", rest::open_repository, "open_repo");
-    repo_router.get("/repo/:repo_id/file", rest::create_file, "create_repo_files");
-    repo_router.get("/repo/:repo_id", rest::list_files, "list_all_repo_files");
-    repo_router.get("/repo/:repo_id/", rest::list_files, "search_all_repo_files");
-    repo_router.get("/repo/:repo_id/:type", rest::list_files, "list_all_repo_files_by_type");
-    repo_router.get("/repo/:repo_id/:type/", rest::list_files, "search_all_repo_files_by_type");
+    router.get("/", rest::ui::index);
+    router.get("/index.html", rest::ui::index);
+    router.get("/manifest.appcache", rest::ui::manifest);
+    router.get("/asset-manifest.json", rest::ui::asset_mainfest);
+    router.get("/favicon.ico", rest::ui::favicon);
+    router.get("/static/*file_name", rest::ui::files);
+    router.get("/*any", rest::ui::any);
 
-
-    #[cfg(debug_assertions)]
-    {
-        repo_router.post("/repo", rest::cors::create_repository, "cors_create_repo");
-        repo_router.post("/repo/:repo_id", rest::cors::open_repo_ping, "cors_open_repo");
-        repo_router.post("/repo/:repo_id/:file_id", rest::cors::create_file, "cors_create_file");
-    }
-
-    let mut ui_router = Router::new();
-    ui_router.get("/", rest::ui::index, "index");
-    ui_router.get("/index.html", rest::ui::index, "index.html");
-    ui_router.get("/manifest.appcache", rest::ui::manifest, "manifest");
-    ui_router.get("/asset-manifest.json", rest::ui::asset_mainfest, "asset-manifest");
-    ui_router.get("/favicon.ico", rest::ui::favicon, "favicon");
-    ui_router.get("/static/*file_name", rest::ui::files, "files");
-    ui_router.get("/*any", rest::ui::any, "redirect_any");
+//    #[cfg(debug_assertions)]
+//    {
+//        router.post("/repo", rest::cors::create_repository, "cors_create_repo");
+//        router.post("/repo/:repo_id", rest::cors::open_repo_ping, "cors_open_repo");
+//        router.post("/repo/:repo_id/:file_id", rest::cors::create_file, "cors_create_file");
+//    }
 
 
-    mount.mount("/rest/v1/repo", repo_router);
-    mount.mount("/", ui_router);
-
-
-    //    let r = rocket::ignite();
-    //    let config = config::active().ok_or(ConfigError::NotFound).unwrap();
-    //let template_dir = PathBuf::from(config.get_str("ui_dir").unwrap());
-    //let repository_dirs: Vec<PathBuf> = config.get_slice("repository_dirs").unwrap().iter().map(|name| PathBuf::from(name.as_str().unwrap())).co
-    //llect();
     let state = GlobalState::new(Vec::new()).unwrap();
     let uidir = UiDir(PathBuf::new());
 
-    let mut chain = Chain::new(mount);
-    chain.link(Read::<GlobalState>::both(state));
-    let iron = Iron::new(chain).http("localhost:8000").unwrap();
+    let addr = "127.0.0.1:8091".parse().unwrap();
+    let s = Server::new(addr, router);
+    s.add_state(state);
+    s.add_state(uidir);
+    s.start_http_blocking().unwrap();
 }
--- src/rest/ui.rs
+++ src/rest/ui.rs
@@ -10,14 +10,11 @@
 use std::io;
 use std::path::{PathBuf, Path};
 use std::io::Cursor;
-use iron::prelude::*;
-use iron::status;
-use persistent::Read;
 use state::UiState;
-use router::Router;
+use rest_in_rust::prelude::*;
 
 //#[get("/")]
-pub fn index(req: &mut Request) -> IronResult<Response> {
+pub fn index(req: &mut Request) -> Result<Response, HttpError> {
     read_single_file("index.html", &mut req)
 }
 
@@ -33,7 +30,7 @@ fn read_file_string(path: &PathBuf) -> Result<String, ::std::io::Error> {
     }
 }
 
-pub fn manifest(req: &mut Request) -> IronResult<Response> {
+pub fn manifest(req: &mut Request) -> Result<Response, HttpError> {
     use iron::headers::ContentType;
 
     let ui_state = req.get::<Read<UiState>>().unwrap().as_ref();
@@ -49,42 +46,42 @@ pub fn manifest(req: &mut Request) -> IronResult<Response> {
 }
 
 //#[get("/static/<file..>", rank = 9)]
-pub fn files(req: &mut Request) -> IronResult<Response> {
+pub fn files(req: &mut Request) -> Result<Response, HttpError> {
     let ui_state = req.get::<Read<UiState>>().unwrap().as_ref();
     let ref file = req.extensions.get::<Router>()
         .unwrap().find("file_name").unwrap_or("/");
 
     let path = ui_state.ui_dir.join("static").join(file);
     let result = read_file_string(path)?;
-    Ok(Response::with((status::Ok, result)))
+    Ok(result.into())
 }
 
-fn read_single_file(name: &str, req: &mut Request) -> IronResult<Response> {
+fn read_single_file(name: &str, req: &mut Request) -> Result<Response, HttpError> {
     let ui_state = req.get::<Read<UiState>>().unwrap().as_ref();
     let path = ui_state.ui_dir.join(name);
     let result = read_file_string(path)?;
-    Ok(Response::with((status::Ok, result)))
+    Ok(result.into())
 }
 
 //#[get("/asset-manifest.json")]
-pub fn asset_mainfest(req: &mut Request) -> IronResult<Response> {
+pub fn asset_mainfest(req: &mut Request) -> Result<Response, HttpError> {
     read_single_file("asset-manifest.json", &mut req)
 }
 
 //#[get("/favicon.ico")]
-pub fn favicon(req: &mut Request) -> IronResult<Response> {
+pub fn favicon(req: &mut Request) -> Result<Response, HttpError> {
     read_single_file("favicon.ico", &mut req)
 }
 
 //#[get("/index.html")]
-pub fn index_html(req: &mut Request) -> IronResult<Response> {
+pub fn index_html(req: &mut Request) -> Result<Response, HttpError> {
     read_single_file("index.html", &mut req)
 }
 
 //pub fn any(any: PathBuf, ui_dir: State<UiDir>) -> Option<NamedFile> {
 //    NamedFile::open(ui_dir.0.join("index.html")).ok()
 //}
-pub fn any(_: &mut Request) -> IronResult<Response> {
+pub fn any(_: &mut Request) -> Result<Response, HttpError> {
     use iron::modifiers::Redirect;
     Ok(Response::with((status::Found, Redirect("/"))))
 }
